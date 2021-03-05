fb5a5266b9df8c56d2e4529abface4f39424b77d
Christian Manriquez <chrmanriquez@gmail.com>
2021-03-05 11:49:45 -0300

    primer commit: Primera parte ejercicio Cars & Cats

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// const session = require('express-session');
+// const flash = require('connect-flash');
+
+const app = express();
+const port = 8000;
+
+// app.use(session({secret: 'mipropiaclave'}));  
+// app.use(flash());
+
+// para los posts
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+// Para las vistas
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// para archivos estaticos
+app.use('/', express.static("static"));
+
+// importar las rutas
+// app.use(require('./routes/auth'));
+// app.use(require('./routes/routes'));
+
+
+// Lanzamos nuestra aplicación
+const server = app.listen(port, function() {
+  console.log('Escuchando en el puerto ' + port);
+});
